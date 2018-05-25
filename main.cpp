1efeeb20e077c7437383f53825d05f196171760c
Grzegorz Gajoch <gajoch@gmail.com>
2018-05-25 02:29:39 +0200

    First solution - DFS based

--- main.cpp
+++ main.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 #include <vector>
 
-int v, e;
-std::vector<std::vector<int>> g;
+int v, e, max_time;
+
+struct Edge {
+    int next;
+    int time;
+
+    Edge(int next, int time) : next(next), time(time) {}
+};
+std::vector<std::vector<Edge>> g;
 
 std::vector<bool> visited;
 
+int best_solution_time;
+std::vector<int> best_solution;
 
-int dfs(int vec, std::vector<int> path) {
+int dfs(int vec, int time, std::vector<int> path) {
 //    printf("IN %d\n", vec);
     bool any = false;
     for(auto next : g[vec]) {
-        if (!visited[next]) {
+        if (time + next.time <= max_time && !visited[next.next]) {
             any = true;
 
-            visited[next] = true;
-            path.push_back(next);
-            dfs(next, path);
+            visited[next.next] = true;
+            path.push_back(next.next);
+            dfs(next.next, time + next.time, path);
             path.pop_back();
 
-            visited[next] = false;
+            visited[next.next] = false;
         }
     }
 
@@ -28,27 +37,37 @@ int dfs(int vec, std::vector<int> path) {
         for(auto x : path) {
             printf("%d ", x);
         }
-        printf("\n");
+        printf("---> %d %d\n", time, path.size());
+
+        if (path.size() > best_solution.size()) {
+            best_solution = path;
+            best_solution_time = time;
+        }
     }
 //    printf("OUT %d\n", vec);
 }
 
 int main() {
-    std::cin >> v >> e;
+    std::cin >> v >> e >> max_time;
 
     visited.resize(v, false);
     g.resize(v);
 
     for(int i = 0; i < e; ++i) {
-        int a, b;
-        std::cin >> a >> b;
-        g[a].push_back(b);
-        g[b].push_back(a);
+        int a, b, t;
+        std::cin >> a >> b >> t;
+        g[a].push_back(Edge(b, t));
+        g[b].push_back(Edge(a, t));
     }
 
     visited[0] = true;
-    dfs(0, {});
+    dfs(0, 0, {0});
 
-    std::cout << "Hello, World!" << std::endl;
+
+    printf("Best solution found [len = %u, time = %d]:\n", best_solution.size(), best_solution_time);
+    for(auto i : best_solution) {
+        printf("%d ", i);
+    }
+    printf("\n");
     return 0;
 }
